150852372f4671562a3cc382eef13f0e829ff835
segunojo1 <olusegunkd2017@gmail.com>
2025-01-05 01:19:32 +0100

    fix: services

--- src/components/landing-page/Services.tsx
+++ src/components/landing-page/Services.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export const Services = () => {
     return (
-        <section className="mt-[128px]" id="services">
+        <section className="mt-[128px] mb-[168px]" id="services">
             <div className="flex items-center justify-between">
                 <h2 className="text-[30px]/[54px] font-bold">Services we offer</h2>
                 <div className="flex items-center gap-[14px]">
@@ -33,10 +33,10 @@ type ServiceType = {
 }
 const Service = ({ img, title, text }: ServiceType) => {
     return (
-        <div className="h-[480px]">
-            <div className="bg-">
-                <Image src={img} width={27} height={27} alt="left" />
-                <h3>{title}</h3>
+        <div className="h-[480px] w-[480px] bg-white">
+            <div className=" bg-gradient-to-b from-[#D9D9D9] bg-blend-overlay to-[#006494] relative">
+                <Image src={img} width={405} height={245} alt="img" />
+                <h3 className="text-[#F4F4F4] text-[28px]/[36px] font-bold absolute left-10 bottom-10">{title}</h3>
             </div>
             <p className="mt-6 text-center">{text}</p>
         </div>
