53b93ac843a86319f8f4de4800fb90879046de7b
BrightDU <brightuto@gmail.com>
2025-01-07 19:54:57 -0800

    fixed some bugs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import favicon from '../app/favicon.ico'; 
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,7 +15,10 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "700 App Solutions",
-  description: "Innovative Solutions for Businesses",
+  description: "Innovation, Solutions, Business, Technology, Apps, Nigeria",
+  icons: {
+    icon: favicon.src, // Use imported favicon
+  },
 };
 
 export default function RootLayout({
@@ -24,6 +28,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+       <head>
+        {/* Add global favicon link for SSR */}
+        <link rel="icon" href={favicon.src} type="image/x-icon" sizes="any" />
+        <meta name="description" content="Innovation, Solutions, Business, Technology, Apps, Nigeria, Saudi" />
+        <title>700 App Solutions</title>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
